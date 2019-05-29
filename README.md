09676188e446e0359569eaabac08eccbbd3955a3
Oege Hiddema <oegehiddema@gmail.com>
2019-05-29 10:03:44 +0200

    connect to database

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ The database consists of one table: laptops.
 
 Customers must be able to search for their perfect laptop, using criteria filters.
 Administrators must be able to add, delete and update the data.
+
+Steps to undertake:
+[x] use the database created in the previous repo
+[ ] connect to the database
+[ ] start with administrator interface:
+   - create a form insert a record into the table
---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+require_once('secret.php'); //contains servername, username and password
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDBPDO", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
