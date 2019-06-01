a402f6c3a9745829456ceb0fae224ba3546dd828
Samir Hartlief <samir.hartlief@hotmail.com>
2019-06-02 01:11:28 +0200

    removed edge related metatag

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ if(isset($_REQUEST['page'])) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <base href="/laptopshop-1/">
         <script type="text/javascript" src="js/script.js"></script>
     </head>
