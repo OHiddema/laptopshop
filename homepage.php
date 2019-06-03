27ff508dadb24254ffebbb71f46364adb80b6b49
Oege Hiddema <oegehiddema@gmail.com>
2019-06-03 10:36:45 +0200

    make index.html single point of entry. Move content of index.html to homepage.html

---
+++ homepage.php
@@ -0,0 +1,5 @@
+<h1>Homepage</h1>
+<hr>
+<!-- <a href="addpage.php">Administrator: Add record</a><br><br> -->
+<a href="?page=viewall.php">I am an administrator</a><br><br>
+<a href="?page=customer.html">I am a customer</a><br><br>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<h1>Homepage</h1>
-<hr>
-<!-- <a href="addpage.php">Administrator: Add record</a><br><br> -->
-<a href="?page=viewall.php">I am an administrator</a><br><br>
-<a href="?page=customer.html">I am a customer</a><br><br>
+<?php
+    if(isset($_REQUEST['page'])) {
+        $page = $_REQUEST['page'].'.php';
+    } else $page = 'homepage.php';
+?>
+
+<?php include($page);?>
