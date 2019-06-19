69b63ae9cdb75dc958e7bceba6be34f495bebaca
Oege Hiddema <oegehiddema@gmail.com>
2019-06-19 12:43:00 +0200

    do not show blnactive field to users

--- getcustfiltdata.php
+++ getcustfiltdata.php
@@ -4,7 +4,6 @@
    header("Content-Type: application/json; charset=UTF-8");
    $obj = json_decode($_POST["x"], false);
 
-   // $query = $conn->prepare('SELECT * FROM laptops WHERE price<=:price AND memory>=:memory');
    $query = $conn->prepare('SELECT * FROM laptops WHERE price<=:price AND memory>=:memory AND blnactive="1"');
    $query->execute(['price' => $obj->maxprijs, 'memory' => $obj->minmem]);
 
@@ -13,22 +12,22 @@
    echo '<table border ="2">';
    echo '<tr>';
 
-   // This is realy cool! Column names are fetched from the database.
-   $cols = $query->columnCount();
-   for ($i = 0; $i<$cols; $i++) {
-      $header = $query->getcolumnMeta($i)['name'];
-      echo '<th>' . $header . '</th>';
-   }
-   
+   echo '<th>id</th>';
+   echo '<th>brand</th>';
+   echo '<th>name</th>';
+   echo '<th>price</th>';
+   echo '<th>memory</th>';   
+
    echo '</tr>';
 
    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 
       echo '<tr>';
-      foreach($row as $field) {
-         echo "<td>" . $field . "</td>";
-      }
-      
+      foreach($row as $key=>$field) {
+         if ($key != 'blnactive') {
+            echo "<td>" . $field . "</td>";
+         }
+      }      
       echo '</tr>';
       
 }
