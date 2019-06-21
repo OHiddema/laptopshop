472105841ebb833543cd6e54e1253fe90bfcac8b
Oege Hiddema <oegehiddema@gmail.com>
2019-06-21 15:42:21 +0200

    insert or update, depending on whether product is already in basket

--- addtobasket.php
+++ addtobasket.php
@@ -7,9 +7,23 @@
    $customer_id =  $_SESSION['logged_in_user_id'];
 
    try {
-      $stmt = $conn->prepare('INSERT INTO basket (product_id, amount, customer_id) VALUES (:product_id, :amount, :customer_id)');
-      $stmt->execute(['product_id' => $product_id, 'amount' => $amount, 'customer_id' => $customer_id]);
-      echo "Basket filled successfully<br>";
+
+      // First, check if product_id already exists
+      $stmt = $conn->prepare('SELECT * FROM basket WHERE customer_id=:customer_id AND product_id=:product_id');
+      $stmt->execute(['customer_id' => $customer_id, 'product_id' => $product_id]);
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      if (!$row) {
+         // product_id does not exist => INSERT
+         $stmt = $conn->prepare('INSERT INTO basket (product_id, amount, customer_id) VALUES (:product_id, :amount, :customer_id)');
+         $stmt->execute(['product_id' => $product_id, 'amount' => $amount, 'customer_id' => $customer_id]);
+         echo "Insert successfully<br>";
+      } else {
+         // product_id does exist => UPDATE
+         $amount += $row['amount'];
+         $stmt = $conn->prepare("UPDATE basket SET amount=:amount WHERE product_id=:product_id AND customer_id=:customer_id");
+         $stmt->execute(['amount' => $amount, 'product_id' => $product_id, 'customer_id' => $customer_id]);
+         echo "Update successfully<br>";
+      }
       }
    catch(PDOException $e)
       {
