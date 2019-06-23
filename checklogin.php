c9ddc3e4b1b517b5c9e785a333f011854c12fc0e
Oege Hiddema <oegehiddema@gmail.com>
2019-06-23 10:01:30 +0200

    rearrange try/catch block in checklogin

--- checklogin.php
+++ checklogin.php
@@ -9,30 +9,28 @@
       $stmt = $conn->prepare('SELECT * FROM customers WHERE username=:username AND password=:password');
       $stmt->execute(['username' => $username, 'password' => $password]);
       $row = $stmt->fetch(PDO::FETCH_ASSOC);
-      if (!$row) {
-         session_destroy();
-
-         // redirect page
-         $page = '?page=login.php';
-
-      } else {
-         $_SESSION['logged_in_user_id'] = $row['id'];
-         $_SESSION['logged_in_user_name'] = $row['username'];
-
-         // redirect page
-         $page = '?page=customer.php';
-      }
-
-      // redirect
-      $host  = $_SERVER['HTTP_HOST'];
-      $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-      header("Location: http://$host$uri/$page");
-      exit;
    }
    catch(PDOException $e)
       {
       echo 'Error fetching username and password' . "<br>" . $e->getMessage();
       }
-  
    $conn = null;
+
+   if (!$row) {
+      session_destroy();
+      // redirection page
+      $page = '?page=login.php';
+
+   } else {
+      $_SESSION['logged_in_user_id'] = $row['id'];
+      $_SESSION['logged_in_user_name'] = $row['username'];
+      // redirection page
+      $page = '?page=customer.php';
+   }
+
+   // redirect
+   $host  = $_SERVER['HTTP_HOST'];
+   $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+   header("Location: http://$host$uri/$page");
+   exit;
 ?>
