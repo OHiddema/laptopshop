f27dc7cfd0d78da347a30d03fe6b59e2945963bd
Oege Hiddema <oegehiddema@gmail.com>
2019-06-27 12:42:57 +0200

    use grid layout on customer page

--- customer.php
+++ customer.php
@@ -33,20 +33,25 @@
    <h1>Filter laptops</h1>
    <hr>
 
-   Maximal price: <input onchange='getdata()' id='maxprijs' type="number" min = "0" max="1000" step="100" value='1000'><br><br>
-   Minimal memory: <input onchange='getdata()' id='minmem' type="number" min = "4" max = "32" step = "4" value='4'><br><br>
+   <div class='myGrid'>
+      
+      <div>
+         Maximal price: <input onchange='getdata()' id='maxprijs' type="number" min = "0" max="1000" step="100" value='1000'><br><br>
+         Minimal memory: <input onchange='getdata()' id='minmem' type="number" min = "4" max = "32" step = "4" value='4'><br><br>
 
-   Category:
-   <select onchange='getdata()' id='category'>
-      <option value="All" selected>All categories</option>
-      <option value="B">Budget</option>
-      <option value="A">Allround</option>
-      <option value="P">Professional</option>
-   </select>
-   <br><br>
+         Category:
+         <select onchange='getdata()' id='category'>
+            <option value="All" selected>All categories</option>
+            <option value="B">Budget</option>
+            <option value="A">Allround</option>
+            <option value="P">Professional</option>
+         </select>
+         <br><br>
+      </div>
 
+      <!-- Select all data when page loads -->
+      <script>getdata();</script>
 
-   <!-- Select all data when page loads -->
-   <script>getdata();</script>
+      <p id='resultset'></p>
 
-   <p id='resultset'></p>
+   </div>
