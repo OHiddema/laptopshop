c806d41d5f1f432447af8527296baac05d25a8d2
Oege Hiddema <oegehiddema@gmail.com>
2019-06-27 15:33:37 +0200

    use font awesome shopping cart

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <script src="https://kit.fontawesome.com/4f88f2fc60.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
         <style>
@@ -34,8 +35,9 @@
                     $size = getBasketSize($conn);
                     if ($size==0) {
                        echo "<p>Your basket is empty!</p>";
-                    } else echo "<a href='?page=basket.php'>Check my basket($size)</a>";
-
+                    } else {
+                        echo "<a href='?page=basket.php'><i class='fas fa-shopping-cart fa-lg'>($size)</a></i>";
+                    }
                     echo "Logged in user: ".$_SESSION['logged_in_user_name'];
                     } else echo "No customer logged in";
                 ?>
