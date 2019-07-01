70c4effd91b07a098a86a3236235178fc2aacd19
Oege Hiddema <oegehiddema@gmail.com>
2019-07-01 14:47:45 +0200

    use function defined in index.php (DRY coding)

--- delete.php
+++ delete.php
@@ -8,7 +8,7 @@ try {
    $stmt = $conn->prepare('SELECT * FROM basket WHERE product_id=:id');
    $stmt->execute(['id' => $id]);
    if ($stmt->rowCount()>0) {
-      echo "<script>alert('Product found in basket. Deletion not allowed!');</script>";
+      echo alert('Product found in basket. Deletion not allowed!');
    } else {
       $stmt = $conn->prepare('DELETE FROM laptops WHERE id=:id');
       $stmt->execute(['id' => $id]);   
