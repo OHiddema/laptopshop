8d3d946c2cf24f2daf048c797e22ed5dfd6a285e
Oege Hiddema <oegehiddema@gmail.com>
2019-07-02 14:12:01 +0200

    style with Bootstrap: editpage form

--- editpage.php
+++ editpage.php
@@ -18,18 +18,47 @@ $conn = null;
 <h1>Update laptop</h1>
 <hr>
 <form action = "?page=updaterecord.php" method = "POST">
+   <!-- id not displayed, but necessary to put in form, for POST -->
    <input type="text", name="id", value="<?= $res['id'] ?>" style="display:none">
-   Brand: <input type="text" name="brand" value="<?= $res['brand'] ?>"><br><br>
 
-   Name: <input type="text" name="name" value="<?= $res['name'] ?>"><br><br>
-   Price: <input type = "number" name = "price" value="<?= $res['price'] ?>"><br><br>
-   Memory (GB): <input type="number" name="memory" min = "4" max = "32" step = "4" value="<?= $res['memory'] ?>"><br><br>
+   <div class="form-group row">
+      <label for="brand" class="col-sm-2 col-form-label">Brand:</label>
+      <div class="col-sm-6">
+         <input type="text" class="form-control form-control-sm" id="brand" name="brand" value="<?= $res['brand'] ?>">
+      </div>
+   </div>
+
+   <div class="form-group row">
+      <label for="name" class="col-sm-2 col-form-label">Name:</label>
+      <div class="col-sm-6">
+         <input type="text" class="form-control form-control-sm" id="name" name="name" value="<?= $res['name'] ?>">
+      </div>
+   </div>
+
+   <div class="form-group row">
+      <label for="price" class="col-sm-2 col-form-label">Price:</label>
+      <div class="col-sm-6">
+         <input type="number" class="form-control form-control-sm" id="price" name="price" value="<?= $res['price'] ?>">
+      </div>
+   </div>
+
+   <div class="form-group row">
+      <label for="memory" class="col-sm-2 col-form-label">Memory (GB):</label>
+      <div class="col-sm-6">
+         <input type="number" class="form-control form-control-sm" id="memory" name="memory" min = "4" max = "32" step = "4" value="<?= $res['memory'] ?>">
+      </div>
+   </div>
    
    <?php
    $checked = ($res['blnactive']==1) ? 'checked' : '';
    ?>
 
-   Active: <input type = "checkbox" name = "blnactive" value="1" <?= $checked ?>><br><br>
+   <div class="form-group row">
+      <label for="blnactive" class="col-sm-2 col-form-label">Active:</label>
+      <div class="col-sm-1">
+         <input type="checkbox" class="form-control form-control-sm" id="blnactive" name="blnactive" value="1" <?= $checked ?>>
+      </div>
+   </div>
 
    <?php
    $b = ($res['category']=='B') ? 'selected' : '';
@@ -37,11 +66,16 @@ $conn = null;
    $p = ($res['category']=='P') ? 'selected' : '';
    ?>
    
-   Category: <select name="category">
-   <option value='B' <?=$b?>>Budget</option>
-   <option value='A' <?=$a?>>Allround</option>
-   <option value='P' <?=$p?>>Professional</option>
-   </select><br><br>
+   <div class="form-group row">
+      <label for="sel1" class="col-sm-2 col-form-label">Category:</label>
+      <div class="col-sm-6">
+         <select class="form-control form-control-sm" id="category" name="category">
+               <option value="B" <?=$b?>>Budget</option>
+               <option value="A" <?=$a?>>Allround</option>
+               <option value="P" <?=$p?>>Professional</option>
+            </select>
+         </div>
+   </div> 
 
    <input type="submit" value = "update">
 </form>
