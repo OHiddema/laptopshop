d641c1347deff923efbd6587c7941fbdc0c1732e
Oege Hiddema <oegehiddema@gmail.com>
2019-07-07 10:49:55 +0200

    revert back previous change

--- adduser.php
+++ adduser.php
@@ -17,8 +17,7 @@
       $host  = $_SERVER['HTTP_HOST'];
       $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
       $page = '?page=login.php';
-      // echo "<script>window.location = 'http://$host$uri/$page' ;</script>";
-      header('Location: http://'.$host.$uri."/".$page);      exit;
+      echo "<script>window.location = 'http://$host$uri/$page' ;</script>";
       }
    catch(PDOException $e)
       {
