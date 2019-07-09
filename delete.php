04286ab8bc07d1f6a5bafcd7f9d3c24cc080cc2b
Oege Hiddema <oegehiddema@gmail.com>
2019-07-09 15:33:09 +0200

    Put message in request, echo not allowed

--- delete.php
+++ delete.php
@@ -9,7 +9,6 @@ try {
    $stmt = $conn->prepare('SELECT * FROM basket WHERE product_id=:id');
    $stmt->execute(['id' => $id]);
    if ($stmt->rowCount()>0) {
-      // echo alert('Product found in basket. Deletion not allowed!');
       redirect('?page=viewall.php&message=Product found in basket. Deletion not allowed!');
    } else {
       $stmt = $conn->prepare('DELETE FROM laptops WHERE id=:id');
